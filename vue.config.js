53e36e18a52a9b098e8732198069b5321fbab30c
jameshua16 <56073320+jameshua16@users.noreply.github.com>
2020-05-13 00:41:40 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
   
 module.exports = {
-
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/sorting/'
-      : '/'
-  }
+    // this will be the URL at which it will be served from
+    // if you're using something like example.com, set it to `/`
+    // for my example, I'm doing this b/c I already have something at `/`
+    publicPath: "/sorting/",
+    // by default this is `dist` which is not what we want here
+    // github lets you serve the `docs` folder as a github page
+    outputDir: "docs"
+  };
